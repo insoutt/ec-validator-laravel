4692b125cac1cb113e5e8dc450dd956dc26d67b3
insoutt <hi@elvisfernando.com>
2024-11-01 14:31:33 -0500

    Add tests

---
+++ tests/CedulaTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Validator;
+
+it('Invalid Cedula', function () {
+    /** @var \TestCase\TestCase $this */
+    $validator = Validator::make(
+        ['myparam' => '1400724651'], 
+        ['myparam' => 'ec_cedula']
+    );
+    $this->assertFalse($validator->passes());
+});
+
+it('Valid Cedula', function () {
+    /** @var \TestCase\TestCase $this */
+    $validator = Validator::make(
+        ['myparam' => '1400724652'], 
+        ['myparam' => 'ec_cedula']
+    );
+    $this->assertTrue($validator->passes());
+});
---
+++ tests/PlacaTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Validator;
+
+it('Invalid Placa', function () {
+    $validator = Validator::make(
+        ['myparam' => 'abc09'], 
+        ['myparam' => 'ec_placa']
+    );
+    $this->assertFalse($validator->passes());
+
+    $validator = Validator::make(
+        ['myparam' => 'TBB7233'], 
+        ['myparam' => 'ec_placa_moto']
+    );
+
+    $this->assertFalse($validator->passes());
+
+    $validator = Validator::make(
+        ['myparam' => 'IX617C'], 
+        ['myparam' => 'ec_placa_car']
+    );
+
+    $this->assertFalse($validator->passes());
+});
+
+it('Valid Placa', function () {
+    $validator = Validator::make(
+        ['myparam' => 'TBB7233'], 
+        ['myparam' => 'ec_placa']
+    );
+
+    $this->assertTrue($validator->passes());
+
+    $validator = Validator::make(
+        ['myparam' => 'IX617C'], 
+        ['myparam' => 'ec_placa']
+    );
+
+    $this->assertTrue($validator->passes());
+});
+
+it('Valid car placa', function () {
+    $validator = Validator::make(
+        ['myparam' => 'TBB7233'], 
+        ['myparam' => 'ec_placa_car']
+    );
+
+    $this->assertTrue($validator->passes());
+});
+
+
+it('Valid moto Placa', function () {
+    $validator = Validator::make(
+        ['myparam' => 'IX617C'], 
+        ['myparam' => 'ec_placa_moto']
+    );
+
+    $this->assertTrue($validator->passes());
+});
